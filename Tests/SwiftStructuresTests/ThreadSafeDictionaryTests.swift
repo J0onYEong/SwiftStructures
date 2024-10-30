c3a80876354ad359bf373189d378760d6a8117ad
Junyeong choi <106458638+J0onYEong@users.noreply.github.com>
2024-10-31 01:14:48 +0900

    [SS-000] LockedDictionary동시성 회피 방법 수정 (#7)
    NSLock이 아닌 ConcurrentQueue에 sync작업 및 barrier플래그를 사용하여 해결

--- Tests/SwiftStructuresTests/ThreadSafeDictionaryTests.swift
+++ Tests/SwiftStructuresTests/ThreadSafeDictionaryTests.swift
@@ -13,23 +13,34 @@ final class ThreadSafeDictionary: XCTestCase {
     func testThreadSaftyForLockedDictionary() {
         let dictionary = LockedDictionary<Int, String>()
         let expectation = XCTestExpectation(description: "Multiple threads access LockedDictionary safely")
-        expectation.expectedFulfillmentCount = 10 // 10개의 스레드가 완료되기를 기다림
+        expectation.expectedFulfillmentCount = 90 // 300개의 완료되기를 기다림
         
-        DispatchQueue.concurrentPerform(iterations: 100) { index in
+        DispatchQueue.concurrentPerform(iterations: 30) { index in
+            
+            let key = index
+            let testValue = "Value \(index)"
+            
             DispatchQueue.global().async {
-                let testValue = "Value \(index)"
-                dictionary[index] = testValue // 쓰기 작업
                 
-                let value = dictionary[index] // 읽기 작업
-                XCTAssertEqual(value, testValue, "Value mismatch in thread \(index)")
+                dictionary[key] = testValue // 쓰기 작업
                 
-                dictionary.remove(key: index) // 삭제 작업
-                XCTAssertNil(dictionary[index]) // 삭제후 값이 없어야함
+                expectation.fulfill() // 스레드 작업 완료
+            }
+            
+            DispatchQueue.global().async {
+                let _ = dictionary[key] // 읽기 작업(실패가능, 런타임 에러만 아니면 통과)
+                
+                expectation.fulfill() // 스레드 작업 완료
+            }
+            
+            DispatchQueue.global().async {
+                dictionary.remove(key: key) // 삭제 작업
+                XCTAssertNil(dictionary[key]) // 삭제후 값이 없어야함
                 
                 expectation.fulfill() // 스레드 작업 완료
             }
         }
         
-        wait(for: [expectation], timeout: 5.0) // 5초 내에 모든 스레드가 완료되기를 기대
+        wait(for: [expectation], timeout: 10.0) // 5초 내에 모든 스레드가 완료되기를 기대
     }
 }
